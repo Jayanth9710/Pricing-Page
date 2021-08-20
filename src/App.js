7fc701f8856024a07da38053e8cdfe24e00b7255
Jayanth9710 <kumarjayanth55@gmail.com>
2021-08-21 00:09:41 +0530

    Heading added.

--- src/App.js
+++ src/App.js
@@ -131,29 +131,7 @@ function App() {
   return (
     <div>
       <header>
-      <nav class="navbar navbar-dark bg-primary ">
-      <div class="container-fluid">
-    <a class="navbar-brand" href="#">Pricing Page</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarText">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Free</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pro</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Plus</a>
-        </li>
-      </ul>
-      
-    </div>
-  </div>
-  
-</nav>
+      <h1 className="heading">Pricing Page</h1>
       </header>
       <section class="pricing py-5">
         <div class="container">
